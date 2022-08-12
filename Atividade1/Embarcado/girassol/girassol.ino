8c57d5709c0f7a73631d15e7bd00c7d9ce761f0e
Eduardo Kamada <edukamada@gmail.com>
2022-08-11 23:29:59 -0300

    Adicionado maquina de estados para calibração de sensores LDR

--- Atividade1/Embarcado/girassol/girassol.ino
+++ Atividade1/Embarcado/girassol/girassol.ino
@@ -3,6 +3,15 @@ Servo Motor;
 
 //DEFINES
 #define VALOR_DEBOUNCE 10
+#define CALIBRATION_DEBOUNCE_CYCLE 1000
+
+enum STATE {
+  CALIBRATION_LEFT_LDR,
+  CALIBRATION_RIGHT_LDR,
+  NORMAL_OPERATION,
+  CHECK_CALIBRATION
+
+};
 
 //VARIÁVEIS
 int leftLdr = 0;
@@ -16,12 +25,26 @@ int leftLdrReal = 0;
 int rightLdrReal = 0;
 int printserial = 100;
 int debounce_angle = 5;
+int calibrationDebounce = CALIBRATION_DEBOUNCE_CYCLE;
+
+int leftLdrMaxValue = 0;
+int rightLdrMaxValue = 0;
+
+int leftLdrMinValue = 0;
+int rightLdrMinValue = 0;
 
 //Protótipo
 void getLdrValues(void);
 void setLdrRealValues(void);
 int getMotorAngle(void);
 void setMotorAngle(int angle);
+void loopingControl(void);
+
+
+
+unsigned char machineState = CALIBRATION_LEFT_LDR;
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -43,26 +66,58 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  getLdrValues();
+  switch (machineState) {
+    case CALIBRATION_LEFT_LDR:
+      getLdrValues();
+      if (leftLdr > 300) {
+        if (calibrationDebounce) {
+          calibrationDebounce--;
+        } else {
+          machineState = CALIBRATION_RIGHT_LDR;
+          leftLdrMaxValue = leftLdr;
+          rightLdrMinValue = rightLdr;
+          calibrationDebounce = CALIBRATION_DEBOUNCE_CYCLE;
+        }
+      } else {
+        calibrationDebounce = CALIBRATION_DEBOUNCE_CYCLE;
+      }
+      Serial.print("Esquerda: ");
+      Serial.println(leftLdr);
+      break;
+    case CALIBRATION_RIGHT_LDR:
+      getLdrValues();
+      if (rightLdr > 300) {
+        if (calibrationDebounce) {
+          calibrationDebounce--;
+        } else {
+          machineState = CHECK_CALIBRATION;
+          leftLdrMinValue = leftLdr;
+          rightLdrMaxValue = rightLdr;
+        }
+      } else {
+        calibrationDebounce = CALIBRATION_DEBOUNCE_CYCLE;
+      }
+      Serial.print("Direita: ");
+      Serial.println(rightLdr);
+      break;
+    case NORMAL_OPERATION:
+      loopingControl();
+      break;
 
-  setLdrRealValues();
+      
+      case CHECK_CALIBRATION:
+      Serial.print("Direita maximo: ");
+      Serial.println(rightLdrMaxValue);
+      Serial.print("Direita minimo: ");
+      Serial.println(rightLdrMinValue);
 
-  if (!debounce_angle) {
-    debounce_angle = VALOR_DEBOUNCE;
-    angle = getMotorAngle();
-  } else {
-    debounce_angle--;
+      Serial.print("Esquerda maximo: ");
+      Serial.println(leftLdrMaxValue);
+      Serial.print("Esquerda maximo: ");
+      Serial.println(leftLdrMinValue);
+      break;
   }
 
-  setMotorAngle(angle);
-
-
-  if (printserial) {
-    printserial--;
-  } else {
-    printserial = 100;
-    Serial.println(angle);
-  }
 
 }
 
@@ -104,4 +159,30 @@ int getMotorAngle(void) {
 
 void setMotorAngle(int angle) {
   Motor.write(angle);
+}
+
+void loopingControl(void) {
+
+  getLdrValues();
+
+  setLdrRealValues();
+
+  if (!debounce_angle) {
+    debounce_angle = VALOR_DEBOUNCE;
+    angle = getMotorAngle();
+  } else {
+    debounce_angle--;
+  }
+
+  setMotorAngle(angle);
+
+
+  if (printserial) {
+    printserial--;
+  } else {
+    printserial = 100;
+    Serial.println(angle);
+  }
+
+
 }
